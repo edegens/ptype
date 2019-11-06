fb8777813f28f54d4e784e0a05fc1f1d4e6c305c
Jacky Chiu <jackyzhanchiu@gmail.com>
2019-11-06 17:21:14 -0500

    init attempt to connect embeded etcd nodes. issue is that the extra nodes are waiting on the server node

--- cmd/ptype/main.go
+++ cmd/ptype/main.go
@@ -2,25 +2,39 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"time"
 
-	"github.com/etcd-io/etcd/raft"
+	"go.etcd.io/etcd/embed"
 )
 
 func main() {
-	storage := raft.NewMemoryStorage()
-	c := &raft.Config{
-		ID:              0x01,
-		ElectionTick:    10,
-		HeartbeatTick:   1,
-		Storage:         storage,
-		MaxSizePerMsg:   4096,
-		MaxInflightMsgs: 256,
+	cfgPath := os.Getenv("ETCD_CONFIG")
+	cfg, err := embed.ConfigFromFile(cfgPath)
+	if err != nil {
+		log.Fatal(err)
 	}
-	n := raft.StartNode(c, []raft.Peer{{ID: 0x02}})
 
-	// TODO figure out how to prove that the nodes are able to communicate
+	e, err := embed.StartEtcd(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer e.Close()
+
+	select {
+	case <-e.Server.ReadyNotify():
+		log.Printf("Server is ready!")
+	case <-time.After(60 * time.Second):
+		e.Server.Stop() // trigger a shutdown
+		log.Printf("Server took too long to start!")
+	}
+
+	ticker := time.NewTicker(2 * time.Second)
 	for {
-		rd := n.Ready()
-		fmt.Printf("%#v\n", rd.Messages)
+		select {
+		case <-ticker.C:
+			fmt.Printf("%#v\n", e.Peers)
+		}
 	}
 }
