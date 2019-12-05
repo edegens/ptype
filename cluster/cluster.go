9f016f3c703df5f0a1d062b9c41fbd777df63cbb
Jacky Chiu <jackyzhanchiu@gmail.com>
2019-12-04 22:11:24 -0500

    add the cluster struct for bootstrapping a node

---
+++ cluster/cluster.go
@@ -0,0 +1,40 @@
+package cluster
+
+import (
+	"context"
+	"fmt"
+
+	"go.etcd.io/etcd/embed"
+)
+
+type Cluster struct {
+	Registry *ServiceRegistry
+}
+
+func Join(ctx context.Context, cfg Config) (*Cluster, error) {
+	if _, err := startEmbeddedEtcd(cfg.etcdConfig); err != nil {
+		return nil, err
+	}
+
+	clientURL := cfg.etcdConfig.LCUrls[0].String()
+	registry, err := NewServiceRegistry(ctx, clientURL)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := registry.Register(ctx, cfg.ServiceName, cfg.NodeName, cfg.Port); err != nil {
+		return nil, err
+	}
+
+	return &Cluster{registry}, nil
+}
+
+func startEmbeddedEtcd(cfg *embed.Config) (*embed.Etcd, error) {
+	e, err := embed.StartEtcd(cfg)
+	if err != nil {
+		return nil, fmt.Errorf("failed to start etcd: %w", err)
+	}
+
+	<-e.Server.ReadyNotify()
+	return e, nil
+}
