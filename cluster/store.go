2c0ec8eba844c547f22ffc957decdc5009614b41
Kirin Rastogi <dipshit.dev@gmail.com>
2020-01-08 18:38:51 -0500

    initial get func with failing test

--- cluster/store.go
+++ cluster/store.go
@@ -4,14 +4,20 @@ import (
 	"context"
 	"fmt"
 	"go.etcd.io/etcd/clientv3"
+	"time"
 )
+
+const storePrefix = "store"
 
 type KVStore struct {
 	kv clientv3.KV
 }
 
 func NewKVStore(ctx context.Context, etcdAddr string) (*KVStore, error) {
-	cfg := clientv3.Config{Endpoints: []string{etcdAddr}}
+	cfg := clientv3.Config{
+		Endpoints:   []string{etcdAddr},
+		DialTimeout: 5 * time.Second,
+	}
 	c, err := clientv3.New(cfg)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create etcd client from addr %v: %w", etcdAddr, err)
@@ -20,4 +26,13 @@ func NewKVStore(ctx context.Context, etcdAddr string) (*KVStore, error) {
 	return &KVStore{
 		kv: clientv3.NewKV(c),
 	}, nil
+}
+
+func (kvs *KVStore) Get(ctx context.Context, key string) (string, error) {
+	getres, err := kvs.kv.Get(ctx, key, defaultGetOptions...)
+	if err != nil {
+		return "", fmt.Errorf("failed to get key %s: %w", key, err)
+	}
+	fmt.Println(getres)
+	return "", nil
 }
--- cluster/store_test.go
+++ cluster/store_test.go
@@ -3,11 +3,40 @@ package cluster
 import (
 	"context"
 	"github.com/stretchr/testify/require"
+	"go.etcd.io/etcd/clientv3"
 	"testing"
+	"time"
 )
 
 func TestNewKVStore(t *testing.T) {
 	store, err := NewKVStore(context.Background(), "")
 	require.NoError(t, err)
 	require.NotNil(t, store)
+}
+
+func TestKVGet(t *testing.T) {
+	cleanEtcdDir(t)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	kvs, err := NewKVStore(ctx, TestEtcdAddr)
+	require.NoError(t, err)
+
+	// set up raw connection for test setup
+	cfg := clientv3.Config{
+		Endpoints:   []string{TestEtcdAddr},
+		DialTimeout: 5 * time.Second,
+	}
+	c, err := clientv3.New(cfg)
+	require.NoError(t, err)
+	defer c.Close()
+
+	tmpKV := clientv3.NewKV(c)
+	expected := "uwu"
+	tmpKV.Put(ctx, "store/raccoon", expected)
+
+	val, err := kvs.Get(ctx, "raccoon")
+	require.NoError(t, err)
+	require.Equal(t, expected, val, "value read back should be the same")
 }
