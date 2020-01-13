e6da9589fe2ec51a0b10ed9a705402e843e44237
Jacky Chiu <jackyzhanchiu@gmail.com>
2020-01-13 13:43:57 -0500

    init simple client wrapper

---
+++ cluster/rpc.go
@@ -0,0 +1,38 @@
+package cluster
+
+import (
+	"context"
+	"fmt"
+	"net/rpc"
+)
+
+type Client struct {
+	*rpc.Client
+	sr    *ServiceRegistry
+	nodes []Node
+}
+
+func NewClient(serviceName string, sr *ServiceRegistry) (*Client, error) {
+	serv, err := sr.Services(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	nodes, ok := serv[serviceName]
+	if !ok || len(nodes) == 0 {
+		return nil, fmt.Errorf("failed to find service %v in the registry", serviceName)
+	}
+
+	// naively pick first element; algorithm for choosing node can be improved
+	dailAddr := fmt.Sprintf("%v:%v", nodes[0].Address, nodes[0].Port)
+	client, err := rpc.DialHTTP("tcp", dailAddr)
+	if err != nil {
+		return nil, fmt.Errorf("failed to dial service address %v: %w", dailAddr, err)
+	}
+
+	return &Client{
+		Client: client,
+		sr:     sr,
+		nodes:  nodes,
+	}, nil
+}
