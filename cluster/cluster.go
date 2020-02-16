6de85051582bbebc96e21ec5d5aa031d806df117
Brian Zhang <brian.k.zhang@gmail.com>
2020-02-16 14:30:29 -0500

    Memberadd Part1 (#42)
    add the ability to add a member through the cluster

--- cluster/cluster.go
+++ cluster/cluster.go
@@ -4,7 +4,12 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"sort"
+	"strings"
+	"time"
 
+	"github.com/coreos/etcd/etcdserver/etcdserverpb"
+	"go.etcd.io/etcd/clientv3"
 	"go.etcd.io/etcd/embed"
 	"go.uber.org/zap"
 )
@@ -12,6 +17,7 @@ import (
 type Cluster struct {
 	Registry Registry
 	Store    *KVStore
+	client   *clientv3.Client
 	etcd     *embed.Etcd
 }
 
@@ -43,10 +49,64 @@ func Join(ctx context.Context, cfg Config) (*Cluster, error) {
 		return nil, err
 	}
 
+	clientCfg := clientv3.Config{
+		Endpoints:   []string{clientURL},
+		DialTimeout: 5 * time.Second,
+	}
+	client, err := clientv3.New(clientCfg)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create etcd client from config: %w", err)
+	}
+
 	return &Cluster{
 		Registry: registry,
+		client:   client,
 		etcd:     e,
 	}, nil
+}
+
+type MemberAddInfo struct {
+	InitialCluster      string
+	InitialClusterState string
+}
+
+func (c *Cluster) MemberAdd(ctx context.Context, name, peerURL string) (*MemberAddInfo, error) {
+	mresp, err := c.client.MemberAdd(ctx, []string{peerURL})
+	if err != nil {
+		return nil, fmt.Errorf("failed to add member with peerURL n%v: %w", peerURL, err)
+	}
+
+	initialClusterStrings := make([]string, 0, 2)
+	for _, member := range mresp.Members {
+		if member.Name == "" && strings.Compare(peerURL, member.PeerURLs[0]) == 0 {
+			initialClusterStrings = append(initialClusterStrings, initialClusterStringFormatter(name, member.PeerURLs[0]))
+		} else {
+			initialClusterStrings = append(initialClusterStrings, initialClusterStringFormatter(member.Name, member.PeerURLs[0]))
+		}
+	}
+	sort.Strings(initialClusterStrings)
+
+	initialCluster := strings.Join(initialClusterStrings, ",")
+
+	mai := &MemberAddInfo{
+		InitialCluster:      initialCluster,
+		InitialClusterState: "existing",
+	}
+
+	return mai, nil
+}
+
+func (c *Cluster) MemberList(ctx context.Context) ([]*etcdserverpb.Member, error) {
+	resp, err := c.client.MemberList(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to retrieve member list: %w", err)
+	}
+
+	return resp.Members, nil
+}
+
+func initialClusterStringFormatter(name, peerURL string) string {
+	return fmt.Sprintf("%s=%s", name, peerURL)
 }
 
 func (c *Cluster) Close() error {
--- cluster/cluster_test.go
+++ cluster/cluster_test.go
@@ -2,12 +2,33 @@ package cluster
 
 import (
 	"context"
+	"fmt"
+	"net/url"
+	"os"
+	"path/filepath"
+	"sort"
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+	"go.etcd.io/etcd/embed"
 )
 
-func TestJoin(t *testing.T) {
+func TestClusterSuite(t *testing.T) {
+	suite.Run(t, new(ClusterSuite))
+}
+
+type ClusterSuite struct {
+	suite.Suite
+}
+
+func (suite *ClusterSuite) SetupTest() {
+	cleanDir(suite.T())
+}
+
+func (suite *ClusterSuite) TestJoin() {
+	t := suite.T()
+
 	cfg, err := ConfigFromFile("./testdata/ping.yml")
 	require.NoError(t, err)
 
@@ -19,9 +40,99 @@ func TestJoin(t *testing.T) {
 	require.NoError(t, err)
 	defer c.Close()
 
-	t.Run("test registery contains expected services", func(t *testing.T) {
+	t.Run("test registry contains expected services", func(t *testing.T) {
 		services, err := c.Registry.Services(ctx)
 		require.NoError(t, err)
 		require.NotEmpty(t, services["ping"])
 	})
+}
+
+func (suite *ClusterSuite) TestMemberAdd() {
+	t := suite.T()
+
+	cfg, err := ConfigFromFile("./testdata/ping.yml")
+	require.NoError(t, err)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	c, err := Join(ctx, cfg)
+	require.NoError(t, err)
+	defer c.Close()
+
+	t.Run("test member list contains expected number of servers", func(t *testing.T) {
+		members, err := c.MemberList(ctx)
+		require.NoError(t, err)
+		require.Equal(t, 1, len(members))
+	})
+
+	t.Run("test member add successfully adds new member", func(t *testing.T) {
+		LPUrl, err := url.Parse("http://127.0.0.1:22380")
+		require.NoError(t, err)
+
+		LCUrl, err := url.Parse("http://127.0.0.1:22379")
+		require.NoError(t, err)
+
+		APUrl, err := url.Parse("http://127.0.0.1:22380")
+		require.NoError(t, err)
+
+		ACUrl, err := url.Parse("http://127.0.0.1:22379")
+		require.NoError(t, err)
+
+		memberName := "node2"
+		memberCfg := embed.NewConfig()
+		memberCfg.Name = memberName
+		memberCfg.Dir = "tmp2"
+		memberCfg.LPUrls = []url.URL{*LPUrl}
+		memberCfg.LCUrls = []url.URL{*LCUrl}
+		memberCfg.APUrls = []url.URL{*APUrl}
+		memberCfg.ACUrls = []url.URL{*ACUrl}
+
+		mai, err := c.MemberAdd(ctx, memberCfg.Name, memberCfg.LPUrls[0].String())
+		require.NoError(t, err)
+
+		initialClusterStrings := []string{
+			initialClusterStringFormatter(cfg.etcdConfig.Name, cfg.etcdConfig.LPUrls[0].String()),
+			initialClusterStringFormatter(memberName, LPUrl.String()),
+		}
+		sort.Strings(initialClusterStrings)
+		initialCluster := fmt.Sprintf("%s,%s", initialClusterStrings[0], initialClusterStrings[1])
+
+		expectedmai := &MemberAddInfo{
+			InitialCluster:      initialCluster,
+			InitialClusterState: "existing",
+		}
+
+		require.Equal(t, expectedmai, mai)
+
+		memberCfg.InitialCluster = mai.InitialCluster
+		memberCfg.ClusterState = mai.InitialClusterState
+
+		e, err := embed.StartEtcd(memberCfg)
+		require.NoError(t, err)
+		defer e.Close()
+
+		members, err := c.MemberList(ctx)
+		require.NoError(t, err)
+		require.Equal(t, 2, len(members))
+	})
+}
+
+func removeDirs(glob string) error {
+	dirs, err := filepath.Glob(glob)
+	if err != nil {
+		return err
+	}
+
+	for _, dir := range dirs {
+		if err := os.RemoveAll(dir); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func cleanDir(t *testing.T) {
+	err := removeDirs("tmp*")
+	require.NoError(t, err)
 }
